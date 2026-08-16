78c4ca91b74ddf0821a75dc51288d303d8eaf692
sapopodesu <stomin21@gmail.com>
2026-08-16 21:13:52 +0900

    Add site URL to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://poke-search.net', // 
+  site: 'https://poke-search.net', // ←この行が必須です
   integrations: [sitemap()]
 });
